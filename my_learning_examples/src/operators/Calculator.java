cb0c2f3e89fcd6f96566cd8e878f14c05272a4bb
Grant Wiswell <GT101boy@gmail.com>
2020-10-20 14:11:54 -0500

    Added project and projects for my own work. Operator example created

---
+++ my_learning_examples/src/operators/Calculator.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Operations.add(78, 26);
+		Operations.subtract(94, 21);
+		Operations.divide(78, 22);
+		Operations.equalTo(45, 45);
+		Operations.equalTo(45, 92);
+		Operations.equalTo("Yogurt", "Yogurt");
+		Operations.equalTo("Yogurt", "yogurt");
+		
+	}
+}
